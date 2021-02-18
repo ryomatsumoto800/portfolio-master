ec59a217104709e25aee68b5c9d3d30f30077509
Ubuntu <ubuntu@ip-172-31-28-241.ap-northeast-1.compute.internal>
2021-02-18 17:28:10 +0000

    first commit

---
+++ js/script.js
@@ -0,0 +1,56 @@
+// PhotoSwipe
+initPhotoSwipeFromDOM(".js-my-gallery");
+
+$(function () {
+
+  //iOS対策
+  //iOSでは疑似要素を含むaタグのリンクは２回タップしないと遷移とページ内スクロールをしないため、
+  //ユーザーエージェント判定でiOSの場合はbodyタグにiosを付与し、対象の疑似要素をdisplay: noneする
+  const ua = navigator.userAgent;
+  if (/iPhone|iPad|iPod/.test(ua)) {
+    $("body").addClass("ios");
+  }
+
+  //Worksのリンクを有効化
+  //スライド（Swiper）内に記載のリンクを有効にするため下記の記述が必要 (;´･ω･)ｳｰﾝ･･･
+  $(".works-url").on("click", "a", function (e) {
+    e.stopPropagation();
+  });
+
+  //ページ内スクロール
+  const $nav = $(".gnav");
+  const navHeight = $nav.outerHeight();
+
+  $('a[href^="#"]').on("click", function () {
+    const href = $(this).attr("href");
+    const target = $(href == "#" || href == "" ? "html" : href);
+    const position = target.offset().top - navHeight;
+    $("html, body").animate(
+      {
+        scrollTop: position,
+      },
+      300,
+      "swing"
+    );
+    return false;
+  });
+
+  //ページトップ
+  $("#js-page-top").on("click", function () {
+    $("body,html").animate(
+      {
+        scrollTop: 0,
+      },
+      300
+    );
+    return false;
+  });
+});
+
+new Vue({
+  el: '#example',
+  data: {
+    activeTab: 'tabs-1',
+  },
+});
+
